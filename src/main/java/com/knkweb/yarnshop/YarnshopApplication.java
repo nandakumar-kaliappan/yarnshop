c828a270aa12ffa1f7629a94cc3a54ebfdf7f01a
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-09-28 17:26:37 +0530

    Initial setup comopleted, closes #1

---
+++ src/main/java/com/knkweb/yarnshop/YarnshopApplication.java
@@ -0,0 +1,14 @@
+package com.knkweb.yarnshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YarnshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YarnshopApplication.class, args);
+        System.out.println("Application Running....1107");
+    }
+
+}
---
+++ src/test/java/com/knkweb/yarnshop/YarnshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knkweb.yarnshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YarnshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
